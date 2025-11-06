803716ac94af222e49425fcf67b708a3fd99e2ab
alamdilshad87 <dilshadand@gmail.com>
2025-11-06 17:25:10 +0530

    Added Menu

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,6 +4,9 @@ public class SnakeAndLadder
 {
     public static void Main(String[] args)
     {
+        Console.WriteLine("Player Starts From 0\n");
+        Console.WriteLine("1 - 6 : Dice Roll");
+        Console.WriteLine(" 0 - No Play \n 1 - Ladder \n 2 - Snake\n\n");
         int pos = 0;
         Console.WriteLine($"Player at {pos} ");
         Random r = new Random();
