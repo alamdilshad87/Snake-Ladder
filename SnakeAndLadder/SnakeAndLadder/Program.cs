9bb96a0acc841e4a17aa07721f6f3a03dee68da6
alamdilshad87 <dilshadand@gmail.com>
2025-11-06 17:16:17 +0530

    Random Dice UC2

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Globalization;
 public class SnakeAndLadder
 {
     public static void Main(String[] args)
     {
         int pos = 0;
         Console.WriteLine($"Player at {pos} ");
+        Random r = new Random();
+        int dice = r.Next(1, 7);
+        Console.WriteLine($"Dice Number: {dice}");
     }
 }
